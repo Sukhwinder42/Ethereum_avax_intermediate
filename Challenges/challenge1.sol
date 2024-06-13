1c14c3eb4e2591492c0a2b5cca75e4262f1c18cf
Sukhwinder42 <114457848+Sukhwinder42@users.noreply.github.com>
2024-06-13 18:44:05 +0530

    Update challenge1.sol

--- Challenges/challenge1.sol
+++ Challenges/challenge1.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier:MIT
+pragma solidity >=0.8.7;
+contract Smart{
+    
+    uint a;
+    uint b;
+    address useraddress;
+
+    constructor(){
+        useraddress = msg.sender;
+    }
+
+    modifier student { 
+        require (useraddress == msg.sender, "only student can access");
+        _;
+    }
+
+    function set (uint _a, uint _b) public {
+        a = _a;
+        b = _b;
+    }
+
+    function get() public view student returns(uint){
+       return a*b;
+    }
+}
 

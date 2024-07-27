5416c985db398e829a87126d2246fd4ec71cdc8d
Sukhwinder42 <114457848+Sukhwinder42@users.noreply.github.com>
2024-07-27 13:55:15 +0530

    Update DegenToken.sol

--- Building on Avalanche(module 4)/contracts/DegenToken.sol
+++ Building on Avalanche(module 4)/contracts/DegenToken.sol
@@ -1,8 +1,20 @@
+// Your task is to create a ERC20 token and deploy it on the Avalanche network for Degen Gaming. The smart contract 
+//should have the following functionality:
+
+// Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. 
+// Only the owner can mint tokens.
+// Transferring tokens: Players should be able to transfer their tokens to others.
+// Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
+// Checking token balance: Players should be able to check their token balance at any time.
+// Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+
 
 contract DegenGamingToken is ERC20, Ownable {
     struct Item {
@@ -13,31 +25,26 @@ contract DegenGamingToken is ERC20, Ownable {
 
     uint256 private nextItemId;
     mapping(uint256 => Item) public items;
-    mapping(uint256 => address) public itemOwners; // Mapping to track item ownership
-    mapping(address => mapping(uint256 => uint256)) public redeemedItems; // Mapping to track redeemed items
+    mapping(address => mapping(uint256 => uint256)) public redeemedItems;
 
     event ItemAdded(uint256 id, string name, uint256 price);
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor() ERC20("Degen", "DGN") {}
 
-    // Minting new tokens, only owner can mint
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    // Burn tokens, anyone can burn their own tokens
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    // Add a new item to the store
     function addItem(string memory name, uint256 price) public onlyOwner {
         items[nextItemId] = Item(nextItemId, name, price);
         emit ItemAdded(nextItemId, name, price);
         nextItemId++;
     }
 
-    // Redeem tokens for in-game items
     function redeem(uint256 itemId) public {
         uint256 itemPrice = items[itemId].price;
         require(itemPrice > 0, "Item does not exist");
@@ -46,7 +53,6 @@ contract DegenGamingToken is ERC20, Ownable {
         redeemedItems[msg.sender][itemId]++;
     }
 
-    // Transfer redeemed items
     function transferRewards(address to, uint256 itemId) public {
         uint256 redeemedItemCount = redeemedItems[msg.sender][itemId];
         require(redeemedItemCount > 0, "Insufficient redeemed items to transfer");
@@ -54,7 +60,6 @@ contract DegenGamingToken is ERC20, Ownable {
         redeemedItems[to][itemId]++;
     }
 
-    // View redeemed items
    function viewRedeemedItems(address account, uint256 itemId) public view returns (string memory itemName, uint256 itemCount) {
         itemCount = redeemedItems[account][itemId];
         if (itemCount == 0) {
